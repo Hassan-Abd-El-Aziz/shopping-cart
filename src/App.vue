4b45ee6c793057c5d264d2202bd1cd76f2e5de2d
Hassan-Abd-El-Aziz <hassanabdelaziz940@gmail.com>
2026-02-04 22:04:33 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <div class="min-h-screen">
+    <header class="bg-white p-4 shadow">
+      <h1 class="text-2xl font-bold">Shopping Cart App</h1>
+      <nav>
+        <ul class="flex flex-wrap gap-2">
+          <li>
+            <RouterLink to="/">Home</RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/cart">Cart</RouterLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <main class="p-4">
+      <RouterView />
+    </main>
+  </div>
+</template>
+
+<style scoped>
+nav a.router-link-exact-active,
+.router-link-active {
+  color: #00bba7;
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+</style>
